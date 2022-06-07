f96ec3f4756c0f8a0479c92967d94c17d7f55f5f
chinmaya255696 <chinmayasahoo.v@gmail.com>
2022-06-07 01:08:35 -0700

    mybookdata

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,32 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+    bookName: {type:String,
+               unique: true,
+             required: true },
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    year:{
+        type : String,
+       default : 2021
+    },
+    tags: [String],
+    authorName: String, 
+     totalPages:Number,
+    stockAvailable:Boolean,
+   }, { timestamps: true });
+  
+   
+
+    
+    module.exports = mongoose.model('BookData', bookSchema) //users
+    
+    
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+
 
 //Validation:
 //require:true
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,15 @@
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const bookModel = require("../models/bookModel")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
-router.post("/createBook", BookController.createBook  )
-
-router.get("/getBooksData", BookController.getBooksData)
-
+router.post("/createBook", async function(req,res))
+let data=req.body
+let savedData= await bookModel.create(data)
+res.send({msg : savedData})
 module.exports = router;
