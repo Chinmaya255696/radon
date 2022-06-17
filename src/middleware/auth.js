b1e191f694d14966014b2bef83e1be3f88177b9f
chinmaya255696 <chinmayasahoo.v@gmail.com>
2022-06-17 03:12:24 -0700

    try&catch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,41 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken");
+
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
+    try{let token = req.headers["x-auth-token"]
+    if(!token) return res.status(401).send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-radon')
 
-    next()
+    if(!decodedToken) return res.status(200).send({status: false, msg:"token is not valid"})
+    }catch(error){
+        res.status(500).send({msg: "Error" , error: error.message})
+    }
+
+    next();
 }
 
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    try{let token = req.headers["x-auth-token"];
+    
+    let decodedToken = jwt.verify(token, "functionup-radon");
+    //userId from which the request is made .In this case message to be posted.
+    let userToBeModified = req.params.userId
+
+    //userId for the logged in user
+    let  userLoggedIn = decodedToken.userId
+    
+    //userId comparision to check if the logged inuser is requesting for their own data
+    if(userToBeModified !=userLoggedIn)return res.status(403).send({status:false , msg: "user logged in is not allowed to modify the requested users data"}) 
+    // retun the updated document
+}catch(error){
+    res.status(500).send({msg: "Error" , error: error.message})
+}
+
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,43 @@
+// const express = require('express');
+// const router = express.Router();
+// const userController= require("../controllers/userController")
+// const auth = require("../middleware/auth")
+
+
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const mid = require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser)
+router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+// //The userId is sent by front end
+router.get("/users/:userId",mid.authenticate, mid.authorise, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/users/:userId",mid.authenticate, mid.authorise, userController.updateUser)
+
+router.delete("/users/:userId", mid.authenticate, mid.authorise,userController.deleteUser)
 
 module.exports = router;
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+// router.post("/users", userController.createUser)
+
+// router.post("/login", userController.loginUser)
+
+// //The userId is sent by front end
+// router.get("/users/:userId", userController.getUserData)
+// router.post("/users/:userId/posts", userController.postMessage)
+
+// router.put("/users/:userId", userController.updateUser)
+// router.delete('/users/:userId', auth, userController.deleteUser)
+
+// module.exports = router;
