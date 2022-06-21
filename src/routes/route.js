e6e785a08921d676470975bef39bf2af52f114af
chinmaya255696 <chinmayasahoo.v@gmail.com>
2022-06-20 23:41:44 -0700

    blog

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
-
-
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-router.get("/cowin/getByDistrict", CowinController.getDistrictsSession)
-
-
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
-router.get("/whetherByCity", CowinController.getWhetherForCity)
-
-router.post("/memeCreation", CowinController. memeHandler)
-
-
 
 
 module.exports = router;
